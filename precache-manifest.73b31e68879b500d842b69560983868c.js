0b294b447a61d711213a67699f9b1c409c7f995f
codenation <codenation@codenations-MacBook-Pro.local>
2019-11-21 11:39:02 +0000

    Updates

---
+++ precache-manifest.73b31e68879b500d842b69560983868c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bd4b4ee2a701498e72cec52760bf2e",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "993c09dd96ecc293632c",
+    "url": "/React-Calculator/static/css/main.73053af2.chunk.css"
+  },
+  {
+    "revision": "d223b82f41f23cafa866",
+    "url": "/React-Calculator/static/js/2.84b4ad95.chunk.js"
+  },
+  {
+    "revision": "993c09dd96ecc293632c",
+    "url": "/React-Calculator/static/js/main.a25392b1.chunk.js"
+  },
+  {
+    "revision": "8757082235150dc4ba42",
+    "url": "/React-Calculator/static/js/runtime-main.80e88c10.js"
+  }
+]);
